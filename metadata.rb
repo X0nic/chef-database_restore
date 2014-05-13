3bfa861fdbb56703615cad9480ae7f80cc2bafb2
Nathan Lee <nathan@globalphobia.com>
2014-05-12 22:47:52 -0600

    have thor manage version

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,5 @@ maintainer       'Nathan Lee'
 maintainer_email 'nathan@globalphobia.com'
 license          'All rights reserved'
 description      'Installs/Configures database_restore'
-long_description 'Installs/Configures database_restore'
-version          '0.1.0'
-
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue "0.1.0"
