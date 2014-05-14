4039ff766f3cd9b8d33e21b98a33bda0fed7d5d3
Nathan Lee <nathan@globalphobia.com>
2014-05-13 22:18:43 -0600

    add mysql as a dep

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures database_restore'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue "0.1.0"
+
+depends 'mysql'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,2 @@
-#
-# Cookbook Name:: database_restore
-# Recipe:: default
-#
-# Copyright (C) 2014 YOUR_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+
+include_recipe 'mysql::server'
