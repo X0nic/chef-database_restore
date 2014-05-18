1f6b495216c6368de6e11eac1c2d27f37bdf7fa8
Nathan Lee <nathan@globalphobia.com>
2014-05-17 21:22:41 -0600

    adding s3

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue "0.1.0"
 
 depends 'mysql'
+depends 'hipsnip-s3cmd'
