ccdccd727b1893949d5fd430d6c556fed4ffdfdc
Nathan Lee <nathan@globalphobia.com>
2014-06-02 21:54:43 -0600

    start over using fog instead based on suggestions from #chef

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,35 +19,19 @@
 require 'chef/shell_out'
 
 include_recipe 'mysql::server'
-include_recipe 'hipsnip-s3cmd'
 
-ruby_block "Grabbing s3 bucket data" do
-  block do
-    require 'chef/mixin/shell_out'
-    extend Chef::Mixin::ShellOut
+# Install the Fog gem dependencies
+#
+value_for_platform_family(
+  [:ubuntu, :debian]               => %w| build-essential libxslt1-dev libxml2-dev |,
+  [:rhel, :centos, :suse, :amazon] => %w| gcc gcc-c++ make libxslt-devel libxml2-devel |
+).each do |pkg|
+  package(pkg) { action :nothing }.run_action(:upgrade)
+end
 
-    s3_path = ''
-    Chef::Log.debug "Searching bucket: #{node[:database_restore][:s3_bucket]}"
-    results = shell_out "s3cmd ls #{node[:database_restore][:s3_bucket]} | awk '{print $2}'"
-    Chef::Log.info "Output: #{results.stdout}"
-
-    dates = results.stdout.each_line.map do |line|
-      d = line.match('(\d{4}(\.\d{2}){5})')[0]
-      date = Date.parse(d)
-      # Chef::Log.info "Output: #{date}"
-      {date: date, path: line.chomp}
-    end
-    Chef::Log.info "Output: #{dates.sort_by{ |d| d[:date] }.last}"
-    newest_s3_path = dates.sort_by{ |d| d[:date] }.last
-
-    Chef::Log.info "s3cmd ls #{newest_s3_path[:path]} | awk '{print $2}'"
-    results = shell_out "s3cmd ls #{newest_s3_path[:path]} | awk '{print $4}'"
-
-    final_file_path = results.stdout
-    Chef::Log.info "Output: #{final_file_path}"
-
-    results = shell_out "cd /tmp && s3cmd get #{final_file_path}"
-    Chef::Log.info "Output: #{results.stdout}"
-    Chef::Log.info "Output Error: #{results.stderr}"
-  end
+# Install the Fog gem for Chef run
+#
+chef_gem("fog") do
+  version '1.12.1'
+  action :install
 end
