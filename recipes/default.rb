9f8dbd0b1797fd002fb497c159a6e02cfadca28a
Nathan Lee <nathan@globalphobia.com>
2014-06-11 22:05:13 -0600

    Will now extract and load downloaded db

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,6 +16,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+include_recipe 'mysql::server'
+include_recipe "database::mysql"
+include_recipe "libarchive::default"
+
 require 'fog'
 
 def s3_database_restore_file(s3_bucket, s3_dir_path, database)
@@ -57,5 +61,52 @@ restore_file = s3_database_restore_file(node[:database_restore][:s3_bucket], nod
 
 remote_file "#{Chef::Config[:file_cache_path]}/#{database_name}.tar" do
   source restore_file
+  notifies :create, "mysql_database[#{database_name}]"
   action :create_if_missing
+end
+
+
+# execute 'load_wordpress_database' do
+# end
+
+mysql_connection_info = {
+  :host     => 'localhost',
+  :username => 'root',
+  :password => node['mysql']['server_root_password']
+}
+
+mysql_database database_name do
+  connection mysql_connection_info
+  action     :create
+end
+
+libarchive_file "#{database_name}.tar" do
+  path "#{Chef::Config[:file_cache_path]}/#{database_name}.tar"
+  extract_to Chef::Config[:file_cache_path]
+  action :extract
+end
+
+libarchive_file "wordpressdb.sql.gz" do
+  path "#{Chef::Config[:file_cache_path]}/wordpressdb/databases/MySQL/wordpressdb.sql.gz"
+  extract_to "#{Chef::Config[:file_cache_path]}/wordpressdb.sql"
+  action :extract
+end
+
+mysql_database 'flush the privileges' do
+  connection mysql_connection_info
+  sql        'flush privileges'
+  action     :query
+end
+
+mysql_database 'load_wordpressdb' do
+  connection mysql_connection_info
+  database_name 'wordpress'
+  sql { ::File.open("#{Chef::Config[:file_cache_path]}/wordpressdb.sql/data").read }
+  action :query
+end
+
+mysql_database_user 'wordpress' do
+  connection mysql_connection_info
+  password   'super_secret'
+  action     :create
 end
